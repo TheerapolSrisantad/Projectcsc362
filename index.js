f88888dcbd1c9a2d364f5d80d5a08f12af5d9639
Theerapol <theerapol.sr66@rsu.ac.th>
2024-11-22 10:13:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const books = [
     {
         title: "ชีวิตไม่ง่ายของนางร้าย LV99 เล่ม 4 (Mg)",
         author: "ซาโตริ ทานาบาตะ",
-        price: "$175.00 บาท",
+        price: "175.00 บาท",
         image: "https://storage.naiin.com/system/application/bookstore/resource/product/202410/626189/1000276347_front_XXL.jpg?imgname=%E0%B8%8A%E0%B8%B5%E0%B8%A7%E0%B8%B4%E0%B8%95%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%87%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%99%E0%B8%B2%E0%B8%87%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%A2-LV99-%E0%B9%80%E0%B8%A5%E0%B9%88%E0%B8%A1-4-(Mg)"
     },
     {
